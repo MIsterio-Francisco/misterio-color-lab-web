bb2ea8e2f2d2aca215e70ec8e67e3b8395e9011d
Filmlight <filmlight@Filmlights-MacBook-Pro.local>
2026-03-03 17:56:34 +0100

    fix(build): correct eleventy asset copying and missing html content

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,18 @@
-module.exports = function(eleventyConfig) {
-  // Pass through copy for CSS, JS, Images, Admin
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("admin");
+module.exports = function (eleventyConfig) {
+  // Use mapping object to copy files from src to the root of _site output
+  eleventyConfig.addPassthroughCopy({
+    "src/css": "css",
+    "src/js": "js",
+    "src/img": "img",
+    "src/admin": "admin",
+    "src/data": "data"
+  });
 
   return {
     dir: {
-      input: "src",          
-      output: "_site",     
-      includes: "_includes"  
+      input: "src",
+      output: "_site",
+      includes: "_includes"
     },
     templateFormats: ["html", "md", "njk"],
     htmlTemplateEngine: "njk",
